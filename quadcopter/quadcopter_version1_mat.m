b55a35322de5034738d80909d154747e1bc3379a
Kyle Bringmans <bringmans.kyle@gmail.com>
2021-05-12 21:31:41 +0200

    lqi and lqr work

--- quadcopter/quadcopter_version1_mat.m
+++ quadcopter/quadcopter_version1_mat.m
@@ -1,102 +1,96 @@
-
-clear all
-load 'references_21'
-%massa quadcopter
-mass = 0.5
-%radius quadcopter
-L = 0.25
-%Propeller lift coefficient 
-k = 3*10^(-6)
-%Propeller drag coefficient
-b = 10^(-7)
-%Acceleration of gravity
-g = 9.81
-%Air friction coefficient
-Kd = 0.25
-%Quadcopter inertia about the x-axis
-Ixx = 5*10^(-3)
-%Quadcopter inertia about the y-axis
-Iyy = 5*10^(-3)
-%Quadcopter inertia about the z-axis
-Izz = 1*10^(-2)
-%Motor constant
-cm = 10^4
-%sum of all u_i
+clear;
+load 'references_21';
+%%
+% massa quadcopter
+mass = 0.5;
+% radius quadcopter
+L = 0.25;
+% Propeller lift coefficient 
+k = 3*10^(-6);
+% Propeller drag coefficient
+b = 10^(-7);
+% Acceleration of gravity
+g = 9.81;
+% Air friction coefficient
+Kd = 0.25;
+% Quadcopter inertia about the x-axis
+Ixx = 5*10^(-3);
+% Quadcopter inertia about the y-axis
+Iyy = 5*10^(-3);
+% Quadcopter inertia about the z-axis
+Izz = 1*10^(-2);
+% Motor constant
+cm = 10^4;
+% sum of all u_i
 UU = 163.5;
+% Sampling time
+Ts = 1/20;
+%%
 
 A = zeros(12);
-A(1,4) = 1;A(2,5) = 1;A(3,6) = 1;A(4,4) = -Kd/mass;A(4,8) =k*cm/mass*UU ;
-A(5,5) = -Kd/mass;A(5,9) =k*cm/mass*UU ;A(6,6) = -Kd/mass;
-A(7,10) = 1;A(8,11) = 1;A(9,12) = 1;%A(10,11) = (Iyy-Izz)/Ixx;A(10,12) = (Iyy-Izz)/Ixx;
-%A(11,10) = (Izz-Ixx)/Iyy;A(11,12) = (Izz-Ixx)/Iyy;A(12,10) = (Ixx-Iyy)/Izz;
-%A(12,11) = (Ixx-Iyy)/Izz
-%comment because the omega are zero, see latex.
+A(1,4) = 1;
+A(2,5) = 1;
+A(3,6) = 1;
+A(4,4) = -Kd/mass;
+A(4,8) = k*cm/mass*UU;
+A(5,5) = -Kd/mass;
+A(5,7) = -k*cm/mass*UU;
+A(6,6) = -Kd/mass;
+A(7,10) = 1;
+A(8,11) = 1;
+A(9,12) = 1;
 
 B = zeros(12,4);
 B(6,:) = k*cm/mass;
-B(10,1) = L*k*cm/Ixx;B(10,3) = -L*k*cm/Ixx;
-B(11,2) = L*k*cm/Iyy;B(11,4) = -L*k*cm/Iyy;
-B(12,1) = b*cm/Izz;B(12,2) = -b*cm/Izz;B(12,3) = b*cm/Izz;B(12,4) = -b*cm/Izz;
+B(10,1) = L*k*cm/Ixx;
+B(10,3) = -L*k*cm/Ixx;
+B(11,2) = L*k*cm/Iyy;
+B(11,4) = -L*k*cm/Iyy;
+B(12,1) = b*cm/Izz;
+B(12,2) = -b*cm/Izz;
+B(12,3) = b*cm/Izz;
+B(12,4) = -b*cm/Izz;
 
 C = zeros(6,12);
-C(1,1)=1;C(2,2)=1;C(3,3)=1;C(4,7)=1;C(5,8)=1;C(6,9)=1;
+C(1,1)=1;
+C(2,2)=1;
+C(3,3)=1;
+C(4,7)=1;
+C(5,8)=1;
+C(6,9)=1;
+
 D = zeros(6,4);
-CC = eye(6);
 
-eig(A) 
-CO = ctrb(A,B)
-rank(CO) % rank(CO) > length(A)
-rank(A)
-
-Q = zeros(12,12);
-Q(1,1) = 0.1;Q(2,2) = 0.1;Q(3,3) = 1000;
-Q(4,4) = 10^(-4);Q(5,5) = 10^(-4);Q(6,6) = 10^(-1);
-Q(7,7) = 10;Q(8,8) = 10;Q(9,9) = 10^(-6);
-Q(10,10) = 10^(-1);Q(11,11) = 10^(-1);Q(12,12) = 10^(-2);
-%Q(
-%Q = eye(12,12);
-%Q(1,1) = 10^(-4);Q(2,2) = 10^(-4);Q(3,3) = 10;
-%Q(7,7) = 10;Q(8,8) = 10;Q(9,9) = 10^(-6);
+Q = diag([0.1 0.1 1000 10^-4 10^-4 10^-1 10 10 10 10^-1 10^-1 10^-2]);
 R = eye(4,4)*0.1;
-sys = ss(A,B,C,zeros(6,4))
+
+sys = ss(A,B,C,zeros(6,4));
+sys = c2d(sys,Ts,'zoh');
 
 K1 = lqr(sys,Q,R);
-K1 = lqr(A,B,Q,R);
-Q_i = eye(18,18);
-%Q(1,1) = 1;Q(2,2) = 1;Q(3,3) = 100;
-R_i = eye(4,4)*0.1;
-%integral_act = lqi(sys,Q_i,R_i)
 
-K = zeros(4,6)
-K(:,1:3) = K1(:,1:3)
-K(:,4:6) = K1(:,7:9)
-%NA = [zeros(6,6) C; zeros(12,6) A]
-%NB = [D;B]
-AA = [A(1:3,4:6) A(1:3,10:12);A(7:9,4:6) A(7:9,10:12)]
-BB = [B(4:6,:); B(10:12,:)]
-CCC = eye(4,6)
+N_tot = [sys.A-eye(12) sys.B; sys.C sys.D] \ [zeros(12,6) ; eye(6)];
+N_x = N_tot(1:12,:);
+N_u = N_tot(13:end,:);
 
-minreal(sys)
-Q = eye(18)
-%Q(15,15)
-N = zeros(18,4)
-R
+%%
+%LQI
+
+Q_i = diag([100 100 10 10 10 10 100 100 100 1 1 10 100 100 100]);
+R_i = eye(4,4)*0.001;
+
+Q = eye(18);
 R = eye(4,4);
-eig([Q N;N' R])
-lqqi = lqi(sys,Q,R,N)
-sys = ss(AA,BB,CCC,zeros(4,4))
+Ki = lqi_custom(sys,Q_i,R_i);
 
-lqqi_system = zeros(4,6)
-lqqi_system(:,1:3) = lqqi(:,3:15)
-%lqi(sys,Q_i,R)
-Q = eye(10,10);
-R = eye(4);
+%%
+% Signal noise covariance
+Rk = diag([2.5e-5 2.5e-5 2.5e-5 7.57e-5 7.57e-5 7.57e-5]);
+% Process noise covariance
+Qk = eye(4);
+[kalmf,L,~,Mx,Z] = kalman(sys,Qk,Rk);
 
-%stability and controlability analysis
-eig(AA) ;
-CO = ctrb(AA,BB);
-rank(CO); % rank(CO) > length(A)
-rank(AA);
+
 
 
 
